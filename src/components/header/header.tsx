bf4bce09107133e50dbb0828cf7ca907f2fc1cad
Atefeh Mohammaddoust <atefe.dev@gmail.com>
2023-02-10 09:45:16 +0330

    feat: add header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <h1 data-testid="navbar/title">Contact List</h1>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
